8280a92462345c614222708c251b6d0c62445b6a
HarleyQuinm <595543107@qq.com>
2018-09-27 09:41:58 +0800

    33

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,16 @@
       <el-main></el-main>
       <el-footer>
           <footer-content></footer-content>
+          <div class="footer-copyright">
+            <p>
+                <span>COPYRIGHT 2014-2019 SHANGHAI HUIZHONG GREEN C.S.R</span>&#x3000;
+                <span>上海惠众绿色公益发展促进中心 版权所有 2014-2019</span>
+            </p>
+            <p>
+                <span>沪ICP 15028111号-1</span>&#x3000;
+                <span>沪公网安备 310105020000208</span>
+            </p>
+          </div>
       </el-footer>
     </el-container>     
   </div>
