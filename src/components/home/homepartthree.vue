6d71a6505b6c0620754476b3748a9e7cd77340d0
HarleyQuinm <595543107@qq.com>
2018-09-29 17:38:30 +0800

    首页进度

---
+++ src/components/home/homepartthree.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="home-action-contain">
+        <div class="home-action-content">
+            <div class="part-title">
+                <div class="title-left">
+                    <p class="title-name">绿账行动</p>
+                    <p class="en-title-name">NEWS NEWS TRENDS</p>
+                </div>
+                <div class="title-right">
+                    <p class="action-choose">
+                        <span class="active">全部</span>
+                        <span>申城动态</span>
+                        <span>行业新闻</span> 
+                    </p>
+                </div>
+            </div>
+            <div class="part-content">
+                <div class="action-content-left">
+                    <div class="action-content-left-top">
+                        <img src="" alt="">
+                    </div>
+                    <div class="action-content-left-bottom">
+
+                    </div>
+                </div>
+                <div></div>
+            </div>
+        </div>    
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
