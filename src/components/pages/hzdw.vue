a49e8fc7942de7fefd72f1a4bb9c5cf0564641e9
miaojing <miaojing.nya@gmail.com>
2018-10-08 14:22:29 +0800

    hzdw

---
+++ src/components/pages/hzdw.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="lv_wrap">
+    <div class="lv_bd_bread">您的位置：绿环保 > <span>合作单位</span></div>
+    <ul class="lv_bd_tab">
+      <li class="active">全部</li>
+      <li>支持单位</li>
+      <li>爱心单位</li>
+      <li>理事单位</li>
+      <li>公益机构单位</li>
+    </ul>
+    <div class="lv_dw_text">
+      <div class="lv_dw_title">绿色账户支持单位</div>
+      <div class="lv_dw_name">中国银行上海市分行</div>
+      <div class="lv_dw_name">上海城投（集团）有限公司</div>
+    </div>
+    <div class="lv_dw_text">
+      <div class="lv_dw_title">绿色账户爱心单位</div>
+      <div class="lv_dw_name">光明乳业股份有限公司</div>
+      <div class="lv_dw_name">上海东方希杰商务有限公司</div>
+      <div class="lv_dw_name">上海聚力传媒技术有限公司</div>
+    </div>
+    <div class="lv_dw_text">
+      <div class="lv_dw_title">上海程胜环保科技有限公司</div>
+      <div class="lv_dw_name">上海森蓝环境资源有限公司</div>
+      <div class="lv_dw_name">上海环境实业有限公司</div>
+      <div class="lv_dw_name">中国银行上海分行</div>
+      <div class="lv_dw_name">上海老港固废综合开发有限公司</div>
+      <div class="lv_dw_name">上海市废弃物管理处</div>
+    </div>
+    <div class="lv_dw_text">
+      <div class="lv_dw_title">绿色账户支持单位</div>
+      <div class="lv_dw_name">中国银行上海市分行</div>
+      <div class="lv_dw_name">上海城投（集团）有限公司</div>
+    </div>
+  </div>
+</template>
+<script>
+	import '@/assets/pages/lvzhanghu.css';
+
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+    }
+	}
+</script>
