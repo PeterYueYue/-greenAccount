d960514e2f28e58fc7630e7267c1fa5fa9be16d1
miaojing <miaojing.nya@gmail.com>
2018-10-11 14:09:02 +0800

    api

--- src/api/api.js
+++ src/api/api.js
@@ -1,38 +1,37 @@
 import axios from 'axios';
 // 测试
-let base='http://180.153.19.162:8081';
+let base = 'http://180.153.19.162:8081';
 
 const configarea = {
-    "version":"1.0",
-	"name":"district",
-}
+  "version": "1.0",
+  "name": "district",
+};
 const configproductlist = {
-    "version":"1.0",
-	"name":"getProductList",
-}
+  "version": "1.0",
+  "name": "getProductList",
+};
 const configgetNewinfobyStyleforuser = {
-    "version":"1.0",
-	"name":"getNewInfoByStyleForUser",
-}
+  "version": "1.0",
+  "name": "getNewInfoByStyleForUser",
+};
 
-export default{
-    UUid(){},
-    Uuid(){
-        return new Date().valueOf()+Math.random()
-    },
-    timestamp(){
-        return new Date().valueOf();
-    },
-    getarea(params) {
-        let config = Object.assign(configarea,params,{"nonce":this.Uuid()},{"timestamp":this.timestamp()});
-        return axios.post(`${base}/api/homepage`, config).then(res => res.data);
-    },
-    getProductList(params){
-        let config = Object.assign(configproductlist,params,{"nonce":this.Uuid()},{"timestamp":this.timestamp()});
-        return axios.post(`${base}/api/homepage`, config).then(res => res.data);
-    },
-    getNewInfoByStyleForUser(params){
-        let config = Object.assign(configgetNewinfobyStyleforuser,params,{"nonce":this.Uuid()},{"timestamp":this.timestamp()});
-        return axios.post(`${base}/api/homepage`, config).then(res => res.data);
-    }
+export default {
+  Uuid() {
+    return new Date().valueOf() + Math.random()
+  },
+  timestamp() {
+    return new Date().valueOf();
+  },
+  getarea(params) {
+    let config = Object.assign(configarea, params, {"nonce": this.Uuid()}, {"timestamp": this.timestamp()});
+    return axios.post(`${base}/api/homepage`, config).then(res => res.data);
+  },
+  getProductList(params) {
+    let config = Object.assign(configproductlist, params, {"nonce": this.Uuid()}, {"timestamp": this.timestamp()});
+    return axios.post(`${base}/api/homepage`, config).then(res => res.data);
+  },
+  getNewInfoByStyleForUser(params) {
+    let config = Object.assign(configgetNewinfobyStyleforuser, params, {"nonce": this.Uuid()}, {"timestamp": this.timestamp()});
+    return axios.post(`${base}/api/homepage`, config).then(res => res.data);
+  },
 }
