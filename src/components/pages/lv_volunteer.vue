a3c8d47592eea26090a4b372cc5e192ad6e9bbd3
miaojing <miaojing.nya@gmail.com>
2018-10-15 13:49:44 +0800

    css icon

--- src/components/pages/lv_volunteer.vue
+++ src/components/pages/lv_volunteer.vue
@@ -5,19 +5,28 @@
       <router-link to="/lv_volunteer_details">
         <div class="lv_volunteer_list">
           <div class="title">公园捡拾垃圾关爱环境</div>
-          <div class="text">义工服务队 宝山区</div>
-          <div class="text">活动日期：2018.09.02至2018.10.20<span class="progress">进行中</span></div>
+          <div class="text"><img src="@/assets/lv_v_icon_heart.png" alt="" class="lv_volunteer_icon">义工服务队<img
+            src="@/assets/lv_v_icon_address.png" alt="" class="lv_volunteer_icon_address">宝山区
+          </div>
+          <div class="text"><img src="@/assets/lv_v_icon_date.png" alt="" class="lv_volunteer_icon">活动日期：2018.09
+            .02至2018.10.20<span class="progress">进行中</span></div>
         </div>
       </router-link>
       <div class="lv_volunteer_list">
         <div class="title">公园捡拾垃圾关爱环境</div>
-        <div class="text">义工服务队 宝山区</div>
-        <div class="text">活动日期：2018.09.02至2018.10.20<span class="progress">进行中</span></div>
+        <div class="text"><img src="@/assets/lv_v_icon_heart.png" alt="" class="lv_volunteer_icon">义工服务队<img
+          src="@/assets/lv_v_icon_address.png" alt="" class="lv_volunteer_icon_address">宝山区
+        </div>
+        <div class="text"><img src="@/assets/lv_v_icon_date.png" alt="" class="lv_volunteer_icon">活动日期：2018.09
+          .02至2018.10.20<span class="progress">进行中</span></div>
       </div>
       <div class="lv_volunteer_list">
         <div class="title">公园捡拾垃圾关爱环境</div>
-        <div class="text">义工服务队 宝山区</div>
-        <div class="text">活动日期：2018.09.02至2018.10.20<span class="finished">已结束</span></div>
+        <div class="text"><img src="@/assets/lv_v_icon_heart.png" alt="" class="lv_volunteer_icon">义工服务队<img
+          src="@/assets/lv_v_icon_address.png" alt="" class="lv_volunteer_icon_address">宝山区
+        </div>
+        <div class="text"><img src="@/assets/lv_v_icon_date.png" alt="" class="lv_volunteer_icon">活动日期：2018.09
+          .02至2018.10.20<span class="finished">已结束</span></div>
       </div>
     </div>
 
