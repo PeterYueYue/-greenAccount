64bdfd6dc7135675aa6603214ce151b0670d22ca
miaojing <miaojing.nya@gmail.com>
2018-10-15 09:45:10 +0800

    exchange_list items

--- src/api/api.js
+++ src/api/api.js
@@ -15,80 +15,97 @@ const configgetNewinfobyStyleforuser = {
   "name": "getNewInfoByStyleForUser",
 };
 const configactnotice = {
-    "version":"1.0",
-	"name":"actnotice",
-}
-const configdoninteglist ={
-    "version":"1.0",
-	"name":"doninteglist5",
-}
-const configarearescount ={
-    "version":"1.0",
-	"name":"arearescount9",
-}
-const configcounthouse ={
-    "version":"1.0",
-	"name":"counthouse9",
-}
-const configareavalidpoint ={
-    "version":"1.0",
-	"name":"areavalidpoint9",
-}
+  "version": "1.0",
+  "name": "actnotice",
+};
+const configdoninteglist = {
+  "version": "1.0",
+  "name": "doninteglist5",
+};
+const configarearescount = {
+  "version": "1.0",
+  "name": "arearescount9",
+};
+const configcounthouse = {
+  "version": "1.0",
+  "name": "counthouse9",
+};
+const configareavalidpoint = {
+  "version": "1.0",
+  "name": "areavalidpoint9",
+};
 const configstatdate = {
   "version": "1.0",
   "name": "statdate",
 };
 const configPositionInfo = {
-    "version":"1.0",
-	"name":"getPositionInfo",
-}
-export default{
-    UUid(){},
-    Uuid(){
-        return new Date().valueOf()+Math.random()
-    },
-    timestamp(){
-        return new Date().valueOf();
-    },
-    getarea(params) {
-        let config = Object.assign(configarea,params,{"nonce":this.Uuid()},{"timestamp":this.timestamp()});
-        return axios.post(`${base}/api/web`, config).then(res => res.data);
-    },
-    getProductList(params){
-        let config = Object.assign(configproductlist,params,{"nonce":this.Uuid()},{"timestamp":this.timestamp()});
-        return axios.post(`${base}/api/web`, config).then(res => res.data);
-    },
-    getNewInfoByStyleForUser(params){
-        let config = Object.assign(configgetNewinfobyStyleforuser,params,{"nonce":this.Uuid()},{"timestamp":this.timestamp()});
-        return axios.post(`${base}/api/web`, config).then(res => res.data);
-    },
-    getActNotice(params){
-        let config = Object.assign(configactnotice,params,{"nonce":this.Uuid()},{"timestamp":this.timestamp()});
-        return axios.post(`${base}/api/web`, config).then(res => res.data);
-    },
-    getdoninteglist5(params){
-        let config = Object.assign(configdoninteglist,params,{"nonce":this.Uuid()},{"timestamp":this.timestamp()});
-        return axios.post(`${base}/api/web`, config).then(res => res.data);
-    },
-    getArearescount(params){
-        let config = Object.assign(configarearescount,params,{"nonce":this.Uuid()},{"timestamp":this.timestamp()});
-        return axios.post(`${base}/api/web`, config).then(res => res.data);
-    },
-    getCounthouse(params){
-        let config = Object.assign(configcounthouse,params,{"nonce":this.Uuid()},{"timestamp":this.timestamp()});
-        return axios.post(`${base}/api/web`, config).then(res => res.data);
-    },
-    getAreavalidpoint(params){
-        let config = Object.assign(configareavalidpoint,params,{"nonce":this.Uuid()},{"timestamp":this.timestamp()});
-        return axios.post(`${base}/api/web`, config).then(res => res.data);
-    },
-    getStatDate(params){
-        let config = Object.assign(configstatdate,params,{"nonce":this.Uuid()},{"timestamp":this.timestamp()});
-        return axios.post(`${base}/api/web`, config).then(res => res.data);
-    },
-    getPositionInfo(params){
-        let config = Object.assign(configPositionInfo,params,{"nonce":this.Uuid()},{"timestamp":this.timestamp()});
-        return axios.post(`${base}/api/web`, config).then(res => res.data);
-    }
+  "version": "1.0",
+  "name": "getPositionInfo",
+};
+const configcaringUnitSubmit = {
+  "version": "1.0",
+  "name": "caringUnit.submit",
+};
+const configgetProductDetail = {
+  "version": "1.0",
+  "name": "getProductDetail",
+};
+
+
+export default {
+  Uuid() {
+    return new Date().valueOf() + Math.random()
+  },
+  timestamp() {
+    return new Date().valueOf();
+  },
+  getarea(params) {
+    let config = Object.assign(configarea, params, {"nonce": this.Uuid()}, {"timestamp": this.timestamp()});
+    return axios.post(`${base}/api/web`, config).then(res => res.data);
+  },
+  getProductList(params) {
+    let config = Object.assign(configproductlist, params, {"nonce": this.Uuid()}, {"timestamp": this.timestamp()});
+    return axios.post(`${base}/api/web`, config).then(res => res.data);
+  },
+  getNewInfoByStyleForUser(params) {
+    let config = Object.assign(configgetNewinfobyStyleforuser, params, {"nonce": this.Uuid()}, {"timestamp": this.timestamp()});
+    return axios.post(`${base}/api/web`, config).then(res => res.data);
+  },
+  getActNotice(params) {
+    let config = Object.assign(configactnotice, params, {"nonce": this.Uuid()}, {"timestamp": this.timestamp()});
+    return axios.post(`${base}/api/web`, config).then(res => res.data);
+  },
+  getdoninteglist5(params) {
+    let config = Object.assign(configdoninteglist, params, {"nonce": this.Uuid()}, {"timestamp": this.timestamp()});
+    return axios.post(`${base}/api/web`, config).then(res => res.data);
+  },
+  getArearescount(params) {
+    let config = Object.assign(configarearescount, params, {"nonce": this.Uuid()}, {"timestamp": this.timestamp()});
+    return axios.post(`${base}/api/web`, config).then(res => res.data);
+  },
+  getCounthouse(params) {
+    let config = Object.assign(configcounthouse, params, {"nonce": this.Uuid()}, {"timestamp": this.timestamp()});
+    return axios.post(`${base}/api/web`, config).then(res => res.data);
+  },
+  getAreavalidpoint(params) {
+    let config = Object.assign(configareavalidpoint, params, {"nonce": this.Uuid()}, {"timestamp": this.timestamp()});
+    return axios.post(`${base}/api/web`, config).then(res => res.data);
+  },
+  getStatDate(params) {
+    let config = Object.assign(configstatdate, params, {"nonce": this.Uuid()}, {"timestamp": this.timestamp()});
+    return axios.post(`${base}/api/web`, config).then(res => res.data);
+  },
+  getPositionInfo(params) {
+    let config = Object.assign(configPositionInfo, params, {"nonce": this.Uuid()}, {"timestamp": this.timestamp()});
+    return axios.post(`${base}/api/web`, config).then(res => res.data);
+  },
+  caringUnitSubmit(params) {
+    let config = Object.assign(configcaringUnitSubmit, params, {"nonce": this.Uuid()}, {"timestamp": this.timestamp()});
+    return axios.post(`${base}/api/web`, config).then(res => res.data);
+  },
+  getProductDetail(params) {
+    let config = Object.assign(configgetProductDetail, params, {"nonce": this.Uuid()}, {"timestamp": this.timestamp()});
+    return axios.post(`${base}/api/web`, config).then(res => res.data);
+  },
 
 }
