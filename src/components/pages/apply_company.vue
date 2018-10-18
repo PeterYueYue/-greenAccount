cdd71dd126eb9ab01a1a744e28ac64672719b082
miaojing <miaojing.nya@gmail.com>
2018-10-18 09:28:13 +0800

    pcrImgSrc

--- src/components/pages/apply_company.vue
+++ src/components/pages/apply_company.vue
@@ -26,6 +26,7 @@
       </el-form-item>
       <el-form-item label="验证码" prop="code">
         <el-input v-model="ruleForm.code" placeholder="请输入验证码" class="apply_input"></el-input>
+        <img :src="pcrImgSrc" alt="" class="apply_pcrImg" @click="pcrImg">
       </el-form-item>
       <el-form-item>
         <el-button type="success" @click="submitForm('ruleForm')" class="apply_btn">立即申请</el-button>
@@ -34,6 +35,7 @@
   </div>
 </template>
 <script>
+  import api from "@/api/api.js";
   import '@/assets/pages/apply.css';
 
   export default {
@@ -52,41 +54,69 @@
         rules: {
           name: [
             {required: true, message: '请输入活动名称', trigger: 'blur'},
-            {min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur'}
+            {min: 2, message: '大于2个中文字符', trigger: 'blur'}
           ],
           address: [
             {required: true, message: '请输入公司地址', trigger: 'blur'},
+            {min: 5, message: '大于5个非特殊符号的字符', trigger: 'blur'}
           ],
           contacts: [
             {required: true, message: '请输入联系人', trigger: 'blur'},
+            {min: 2, message: '大于2个非特殊符号的字符', trigger: 'blur'}
           ],
           tel: [
             {required: true, message: '请输入联系电话', trigger: 'blur'},
           ],
           email: [
-            { required: true, message: '请输入邮箱地址', trigger: 'blur' },
-            { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
+            {required: true, message: '请输入邮箱地址', trigger: 'blur'},
+            {type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change']}
           ],
           desc: [
-            { required: true, message: '请填写公司简介', trigger: 'blur' }
+            {required: true, message: '请填写公司简介', trigger: 'blur'},
+            {min: 5, message: '大于5个非特殊符号的字符', trigger: 'blur'}
           ],
           cause: [
-            { required: true, message: '请填写申请事由', trigger: 'blur' }
+            {required: true, message: '请填写申请事由', trigger: 'blur'},
+            {min: 5, message: '大于5个非特殊符号的字符', trigger: 'blur'}
           ],
         },
+        pcrImgSrc: ''
       }
+    },
+    mounted() {
+      this.pcrImg();
     },
     methods: {
       submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            alert('submit!');
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
+        api.caringUnitSubmit({
+          data: {
+            "id": "-1",
+            "cname": "1231212",
+            "address": "123123",
+            "contacts": "123123",
+            "contectsTel": "13999999999",
+            "email": "123@qq.com",
+            "scale": "fwfdsdf",
+            "content": "sdfsd",
+            "yzm": "r7p7",
+          },
+        }).then(res => {
+          this.$refs[formName].validate((valid) => {
+            if (valid) {
+              alert('submit!');
+            } else {
+              console.log('error submit!!');
+              return false;
+            }
+          });
+        })
       },
+      pcrImg() {
+        api.pcrimg({}).then(res => {
+          this.pcrImgSrc = 'data:image/png;base64,' + res.data
+        })
+      },
+
     }
   }
 </script>
