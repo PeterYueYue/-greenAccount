e13b3750b34baf81067b79ae95ce980909c5a084
miaojing <miaojing.nya@gmail.com>
2018-10-19 15:55:24 +0800

    css

--- src/components/pages/lvzhanghu.vue
+++ src/components/pages/lvzhanghu.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="lv_wrap lv_bd_bread">您的位置：绿账户 > <span>绿账行动</span></div>
+    <div class="lv_wrap lv_bd_bread">您的位置：绿账户 > <span>绿色账户</span></div>
     <div class="lv_wrap lv_title">{{listData.newsTitle}}</div>
     <div class="lv_wrap lv_details_head">{{listData.newsNearestUpdateTime | moment}} 来源：{{listData.newsSource}}</div>
     <div class="lv_wrap lv_text" v-html="listData.newsContent"></div>
