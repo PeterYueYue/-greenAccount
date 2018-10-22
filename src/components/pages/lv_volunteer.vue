78fec7005c9838adb11bfdc1ac0d019ea895fff1
miaojing <miaojing.nya@gmail.com>
2018-10-22 14:12:11 +0800

    志愿者列表暂无数据

--- src/components/pages/lv_volunteer.vue
+++ src/components/pages/lv_volunteer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="lv_wrap">
     <div class="lv_volunteer_bread">您的位置：绿互动 > <span>志愿者活动</span></div>
-    <div class="lv_volunteer_tab">
+    <div class="lv_volunteer_tab" v-show="listData.length !== 0">
       <div class="lv_volunteer_list" v-for="(items) in listData">
         <router-link :to="{path: '/lv_volunteer_details/', query: { id: items[5] }}">
           <div class="title">{{items[1]}}</div>
@@ -10,14 +10,14 @@
           </div>
           <div class="text">
             <img src="@/assets/lv_v_icon_date.png" alt="" class="lv_volunteer_icon">活动日期：{{items[3
-            ]}}<span class="progress" v-show="items[4]==='进行中'">进行中</span><span class="finished"
-                                                                                v-show="items[4]==='已结束'">已结束</span>
+            ]}}<span class="progress" v-show="items[4]==='进行中'">进行中</span><span class="finished" v-show="items[4]==='已结束'">已结束</span>
           </div>
         </router-link>
       </div>
     </div>
 
-    <pagination></pagination>
+    <div class="lv_nodata" v-show="listData.length == 0">暂无数据</div>
+    <pagination v-show="listData.length !== 0"></pagination>
   </div>
 </template>
 <script>
