61c91e5f8101e42a3d7a63693a5054c57542a8c6
miaojing <miaojing.nya@gmail.com>
2018-10-29 09:23:13 +0800

    新闻tab分页

--- src/components/pages/zhzq.vue
+++ src/components/pages/zhzq.vue
@@ -2,10 +2,10 @@
   <div class="lv_wrap">
     <div class="lv_bd_bread">您的位置：绿互动 > <span>中行专区</span></div>
     <ul class="lv_bd_tab">
-      <li :class="activeIndex===0?'active':''" @click="allList4NewStyle(0)">全部</li>
-      <li :class="activeIndex===1?'active':''" @click="allList4NewStyle(1)">套卡升级(申请）</li>
-      <li :class="activeIndex===2?'active':''" @click="allList4NewStyle(2)">绿色72色</li>
-      <li :class="activeIndex===3?'active':''" @click="allList4NewStyle(3)">线下活动</li>
+      <li :class="activeIndex===0?'active':''" @click="allList4NewStyle(0,1,5)">全部</li>
+      <li :class="activeIndex===1?'active':''" @click="allList4NewStyle(1,1,5)">套卡升级(申请）</li>
+      <li :class="activeIndex===2?'active':''" @click="allList4NewStyle(2,1,5)">绿色72色</li>
+      <li :class="activeIndex===3?'active':''" @click="allList4NewStyle(3,1,5)">线下活动</li>
     </ul>
     <div class="lv_bd_notice" v-for="(items,index) in listData" @mouseenter.stop="listHover(true,index)"
          @mouseleave.stop="listHover(false,index)" v-show="listData.length !== 0">
@@ -20,27 +20,40 @@
       </router-link>
     </div>
     <div class="lv_nodata" v-show="listData.length == 0">暂无数据</div>
-    <pagination v-show="listData.length !== 0"></pagination>
+    <div class="pagination_wrap">
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="pageCount"
+        :page-size="pageSize"
+        @current-change="pageChange"
+        :current-page.sync="startPage"
+        v-show="listData.length !== 0">
+      </el-pagination>
+    </div>
   </div>
 </template>
 <script>
   import api from "@/api/api.js";
   import '@/assets/pages/lvzhanghu.css';
-  import pagination from '@/components/common/pagination.vue';
+  import '@/components/common/pagination.css';
 
   export default {
     data() {
       return {
         listData: [],
-        activeIndex: 0
+        activeIndex: 0,
+        pageCount: 0,    //总条数
+        pageSize: 5,     //每页条数
+        startPage: 1,    //当前页
       }
     },
-    components: {pagination},
     mounted() {
-      this.allList4NewStyle(0);
+      this.activeIndex = this.$route.params.index || 0;
+      this.allList4NewStyle((this.$route.params.index || 0), 1, 5);
     },
     methods: {
-      allList4NewStyle(status) {
+      allList4NewStyle(status, startPage, pageSize, jumpPage) {
         let category = '';
         switch (status) {
           case 0:
@@ -62,14 +75,23 @@
         api.allList4NewStyle({
           data: {
             category: category,
+            startPage: startPage,
+            pageSize: pageSize,
           },
         }).then(res => {
           res.data.newsList.content.map(items => {
             items.hoverShow = false;
           });
           this.listData = res.data.newsList.content;
-          this.activeIndex = status
+          this.activeIndex = status;
+          this.pageCount = res.data.newsList.totalElements;
+          if (!jumpPage) {
+            this.startPage = 1
+          }
         })
+      },
+      pageChange(startPage) {
+        this.allList4NewStyle(this.activeIndex, startPage, this.pageSize, true);
       },
       listHover(status, index) {
         this.listData[index].hoverShow = status;
