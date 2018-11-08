d9104387c62ef40434ecb349640e7262bb72f944
miaojing <miaojing.nya@gmail.com>
2018-11-08 14:52:14 +0800

    志愿者详情跳转新标签

--- src/components/pages/lv_volunteer.vue
+++ src/components/pages/lv_volunteer.vue
@@ -3,7 +3,7 @@
     <div class="lv_volunteer_bread">您的位置：绿互动 > <span>志愿者活动</span></div>
     <div class="lv_volunteer_tab" v-show="listData.length !== 0">
       <div class="lv_volunteer_list" v-for="(items) in listData">
-        <router-link :to="{path: '/lv_volunteer_details/', query: { id: items[5] }}">
+        <router-link tag="a" target="_blank" :to="{path: '/lv_volunteer_details/', query: { id: items[5] }}">
           <div class="title">{{items[1]}}</div>
           <div class="text"><img src="@/assets/lv_v_icon_heart.png" alt="" class="lv_volunteer_icon">{{items[6]}}<img
             src="@/assets/lv_v_icon_address.png" alt="" class="lv_volunteer_icon_address">{{items[2]}}
